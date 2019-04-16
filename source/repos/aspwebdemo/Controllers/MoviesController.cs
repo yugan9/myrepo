86fd2fa513dba429679f53f4c4dd861dad6edffa
yugan9 <yugandhar.malisetti8@gmail.com>
2019-04-16 12:58:14 +0530

    this is a aspweb projects/

---
+++ source/repos/aspwebdemo/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using aspwebdemo.Models;
+using aspwebdemo.ViewModel;
+
+namespace aspwebdemo.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+       public ActionResult Random()     
+        {
+            var movies = new Movies() { Name = "Panja" };
+            var customers = new List<Customer>
+            {
+                new Customer{Name="Customer 1"},
+                new Customer{Name="Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                movies = movies,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+ 
+    }
+}
---
+++ source/repos/aspwebdemo/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using aspwebdemo.Models;
+
+namespace aspwebdemo.ViewModel
+{
+    public class RandomMovieViewModel
+    {
+        public Movies movies { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
